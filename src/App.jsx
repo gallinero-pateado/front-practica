b0141b3d6f361bdf69c939cc3aa71f09bd1a2000
ConyMJ <cmartinezj@utem.cl>
2024-11-27 15:59:26 -0300

    carpeta de components_login eliminada, ahora la pagina de inicio es search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components_login/layout'; // Layout para login
-import Login from './components_login/login'; //Login para estudiante
-import Register from './components_login/register'; //Register para estudiante
-import LoginEm from './components_login/login_em'; //Login para empresa
-import RegisterEm from './components_login/register_em'; //Register para empresa
-import CompleteProfile from './components_login/complete_profile';
-import PasswordResetForm from './components_login/password_recovery';
-import CompleteProfileEmpresa from './components_login/complete_profile_em';
 
 import UserProfile from './components_profile/user-profile';
 import EditProfile from './components_profile/edit-profile';
@@ -37,25 +29,14 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Rutas con el primer Layout */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login_em" element={<LoginEm />} />
-          <Route path="register_em" element={<RegisterEm />} />
-          <Route path="complete_profile" element={<CompleteProfile />} />
-          <Route path="password_recovery" element={<PasswordResetForm />} />
-          <Route path="complete_profile_em" element={<CompleteProfileEmpresa />} />
 
-
-        </Route>
+        <Route path="/" element={<Layout2><PracticasList /></Layout2>} />
 
         {/* Rutas con el segundo Layout (Layout2) */}
         <Route path="/user-profile" element={<Layout2><UserProfile /></Layout2>} />
         <Route path="/edit-profile" element={<Layout2><EditProfile /></Layout2>} />
         <Route path="/logout" element={<Layout2><Logout /></Layout2>} />
         <Route path="/rpractica" element={<Layout2><Rpractica /></Layout2>} />
-        <Route path="/search" element={<Layout2><PracticasList /></Layout2>} />
         <Route path="/postulacion_practica" element={<Layout2><PostulacionPractica /></Layout2>} />
         <Route path="/leerforo" element={<Layout2><TemasList /></Layout2>} />
         <Route path="/creartema" element={<Layout2><CrearTemaForm /></Layout2>} />
