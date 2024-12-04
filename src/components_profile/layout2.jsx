8129bce993d2f244fd00d19e5e9c60ee64629e6b
Xin0tr3 <mcabreraq@utem.cl>
2024-12-04 19:08:06 -0300

    Logo y redireccion
    Se añadio el logo y redireccion a la unificacion

--- src/components_profile/layout2.jsx
+++ src/components_profile/layout2.jsx
@@ -25,12 +25,12 @@ const Layout2 = ({ children }) => {
 
     useEffect(() => {
         const token = Cookies.get("authToken") || undefined;
-    
+
         if (!token) {
-          // Redirigir si el token no existe
-          window.location.href = "https://ulink.tssw.info";
+            // Redirigir si el token no existe
+            window.location.href = "https://ulink.tssw.info";
         }
-      }, []);
+    }, []);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -65,7 +65,14 @@ const Layout2 = ({ children }) => {
             {/* Header */}
             <header className="bg-[#0092BC] text-white p-6 relative z-20">
                 <div className="flex justify-between items-center mx-auto">
-                    <Link to="/" className="text-5xl font-bold italic">ULINK</Link>
+                    <h1 className="text-5xl font-bold italic">
+                        <div className="flex items-center space-x-4">
+                            <img src="./logo-utem.png" alt="Logo de Ulink" className="h-20" />
+                            <a href="https://ulink.tssw.info/unificacion" className="hover:no-underline">
+                                ULINK
+                            </a>
+                        </div>
+                    </h1>
                     <div className="flex items-center space-x-4">
                         {/* Dark Mode Toggle */}
                         <div
